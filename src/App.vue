f0ec6e18ff4e07fc18f1768a3fbec27b26bd1ff6
데스크탑 <z_gahu@naver.com>
2025-01-26 23:49:31 +0900

    숙제 답안보고 수정

--- src/App.vue
+++ src/App.vue
@@ -33,20 +33,19 @@ export default {
   data() {
     return {
       게시물: postdata,
-      다음데이터: 0,
+      더보기: 0,
     };
   },
   methods: {
     more() {
-      const url =
-        "https://codingapple1.github.io/vue/more" + this.다음데이터 + ".json";
-      console.log(" 눌렀을때 addData", this.다음데이터, url);
-      axios.get(url).then((결과) => {
-        //요청 성공시 실행할 코드
-        console.log("결과", 결과.data);
-        this.게시물.push(결과.data);
-        this.다음데이터 = ++this.다음데이터;
-      });
+      axios
+        .get(`https://codingapple1.github.io/vue/more${this.더보기}.json`)
+        .then((결과) => {
+          //요청 성공시 실행할 코드
+          console.log("결과", 결과.data);
+          this.게시물.push(결과.data);
+          this.더보기++;
+        });
     },
   },
 };
